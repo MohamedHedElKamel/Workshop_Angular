5fb0138b0401a4820da0dc06d478f9f8f5a75bb4
Hedi <ElKamel.MohamedHedi@esprit.tn>
2025-12-01 00:37:52 +0100

    Workshop_2

---
+++ src/app/features/suggestions/suggestion-details/suggestion-details.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Suggestion } from '../../../models/suggestion';
+
+@Component({
+  selector: 'app-suggestion-details',
+  templateUrl: './suggestion-details.component.html',
+  styleUrls: ['./suggestion-details.component.css']
+})
+export class SuggestionDetailsComponent implements OnInit {
+
+  suggestion?: Suggestion;
+
+  suggestions: Suggestion[] = [
+    {
+      id: 1,
+      title: 'Organiser une journée team building',
+      description: 'Suggestion pour organiser une journée de team building pour renforcer les liens entre les membres de l\'équipe.',
+      category: 'Événements',
+      date: new Date('2025-01-20'),
+      status: 'acceptee',
+      likes: 0
+    },
+    {
+      id: 2,
+      title: 'Améliorer le système de réservation',
+      description: 'Proposition pour améliorer la gestion des réservations en ligne avec un système de confirmation automatique.',
+      category: 'Technologie',
+      date: new Date('2025-01-15'),
+      status: 'refusee',
+      likes: 0
+    },
+    {
+      id: 3,
+      title: 'Créer un système de récompenses',
+      description: 'Mise en place d\'un programme de récompenses pour motiver les employés.',
+      category: 'Ressources Humaines',
+      date: new Date('2025-01-25'),
+      status: 'refusee',
+      likes: 0
+    },
+        {
+      id: 4,
+      title: 'Créer un système de récompenses',
+      description: 'Mise en place d\'un programme de récompenses pour motiver les employés.',
+      category: 'Ressources Humaines',
+      date: new Date('2025-01-25'),
+      status: 'refusee',
+      likes: 0
+    },
+        {
+      id: 5,
+      title: 'Créer un système de récompenses',
+      description: 'Mise en place d\'un programme de récompenses pour motiver les employés.',
+      category: 'Ressources Humaines',
+      date: new Date('2025-01-25'),
+      status: 'refusee',
+      likes: 0
+    },
+        {
+      id: 6,
+      title: 'Créer un système de récompenses',
+      description: 'Mise en place d\'un programme de récompenses pour motiver les employés.',
+      category: 'Ressources Humaines',
+      date: new Date('2025-01-25'),
+      status: 'refusee',
+      likes: 0
+    }
+  ];
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.suggestion = this.suggestions.find(s => s.id === id);
+  }
+}
